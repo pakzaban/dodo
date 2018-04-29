f7afe9c276755bc684c186c5a25965438456177e
Peyman Pakzaban <peyman@peymans-mac-mini.local>
2018-04-29 18:10:35 -0500

    trying to integrate user default storage with the item array.

--- dodo/Controllers/ToDoListViewController.swift
+++ dodo/Controllers/ToDoListViewController.swift
@@ -10,47 +10,73 @@ import UIKit
 
 class ToDoListViewController: UITableViewController {
 
-    var itemArray = ["do first", "do next", "do last"]
-    
+    var itemArray = [Item]()
     let defaults = UserDefaults.standard //instantiates the userDefault class for storage of persistent values on the local device
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        if let items = defaults.array(forKey: "toDoArray") as? [String]{
+        
+        let newItem = Item()
+        newItem.title = "do first"
+        itemArray.append(newItem)
+        
+        let newItem2 = Item()
+        newItem2.title = "do next"
+        itemArray.append(newItem2)
+        
+        let newItem3 = Item()
+        newItem3.title = "do last"
+        itemArray.append(newItem3)
+        
+        //retrive itemArray from persistent local storage
+        if let items = defaults.array(forKey: "toDoArray") as? [Item]{
             itemArray = items
         }
     }
     @IBAction func createToDoPressed(_ sender: UIBarButtonItem) {
         var textField = UITextField() //creatimg a local varaiable witha bigger reach than alertTextField
         
+        // 1 & 2. to create a pop-up alert, must first define the controller and the action button
         let alert = UIAlertController(title: "Add a new item to do.", message: "", preferredStyle: .alert)
+        
         let action = UIAlertAction(title: "Add Item", style: .default) { (action) in
-            //creates button on po-up alert.  The completion handler describes what that button does.
-            print(textField.text as Any)
-            self.itemArray.append(textField.text!)
+            //The completion handler describes what that button does.
+            let newItem  = Item()
+            newItem.title = textField.text!
+            self.itemArray.append(newItem)
             self.tableView.reloadData()
             
-            self.defaults.set(self.itemArray, forKey: "toDoArray")
-            
-            
+            self.defaults.set(self.itemArray, forKey: "toDoArray")//stores it locally and persistently
         }
-        
+        // 3. must add action (defined above) and the text field to the AlertController
         alert.addAction(action)
-        
         
         alert.addTextField { (alertTextField) in
             //alertTextField is only available inside this closure
             alertTextField.placeholder = "new item"
             textField = alertTextField
         }
-        
+        // 4. finally, must present the AlertController
         present(alert, animated: true, completion: nil)
         
     }
     
     override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        
         let cell = tableView.dequeueReusableCell(withIdentifier: "ToDoItemCell", for: indexPath)
-        cell.textLabel?.text = itemArray[indexPath.row]
+        // this cell has textLabel and accessoryType properties that can be set
+        
+        let myItem = itemArray[indexPath.row]//defined during refactoring
+        
+        cell.textLabel?.text = myItem.title
+        
+        if myItem.done == true {
+            cell.accessoryType = .checkmark
+        }
+        else{
+            cell.accessoryType = .none
+        }
+        
         return cell
     }
     override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
@@ -58,13 +84,10 @@ class ToDoListViewController: UITableViewController {
     }
     
     override func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
-//        print(itemArray[indexPath.row])
-        if tableView.cellForRow(at: indexPath)?.accessoryType == .checkmark{
-            tableView.cellForRow(at: indexPath)?.accessoryType = .none
-        }
-        else{
-            tableView.cellForRow(at: indexPath)?.accessoryType = .checkmark
-        }
+        //toggle the fone property of the selected item AND reloadData to make tableView update its data
+        self.itemArray[indexPath.row].done = !self.itemArray[indexPath.row].done
+        self.tableView.reloadData()
+        
         tableView.deselectRow(at: indexPath, animated: true)
     }
     
---
+++ dodo/Data Model/Item.swift
@@ -0,0 +1,14 @@
+//
+//  Item.swift
+//  dodo
+//
+//  Created by Peyman PAKZABAN on 4/29/18.
+//  Copyright © 2018 Peyman PAKZABAN. All rights reserved.
+//
+
+import Foundation
+class Item{
+    var title : String = ""
+    var done : Bool = false
+    
+}

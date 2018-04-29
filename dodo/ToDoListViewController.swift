b40be71bf8176030916ccc7f63abd3550d194e0a
Peyman Pakzaban <peyman@peymans-mac-mini.local>
2018-04-29 14:27:42 -0500

    static to do list. Made a table view and created checkmark accessory.

---
+++ dodo/ToDoListViewController.swift
@@ -0,0 +1,43 @@
+//
+//  ViewController.swift
+//  dodo
+//
+//  Created by Peyman PAKZABAN on 4/29/18.
+//  Copyright © 2018 Peyman PAKZABAN. All rights reserved.
+//
+
+import UIKit
+
+class ToDoListViewController: UITableViewController {
+
+    let itemArray = ["do first", "do next", "do last"]
+    
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "ToDoItemCell", for: indexPath)
+        cell.textLabel?.text = itemArray[indexPath.row]
+        return cell
+    }
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return itemArray.count
+    }
+    
+    override func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+//        print(itemArray[indexPath.row])
+        if tableView.cellForRow(at: indexPath)?.accessoryType == .checkmark{
+            tableView.cellForRow(at: indexPath)?.accessoryType = .none
+        }
+        else{
+            tableView.cellForRow(at: indexPath)?.accessoryType = .checkmark
+        }
+        tableView.deselectRow(at: indexPath, animated: true)
+    }
+    
+
+}
+
